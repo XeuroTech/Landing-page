9fe0b3ec3e8497b4eef62950980a317599443d45
SamiTahir09 <samitahir858@gmail.com>
2025-05-12 02:21:42 -0700

    sami

--- src/Pages/Mainpage.jsx
+++ src/Pages/Mainpage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import AiAssistant from '../Components/MainPageCom/AiAssistant'
-
+import BrainPower from '../Components/MainPageCom/BrainPower'
+import Meeting from '../Components/MainPageCom/Meeting'
 
 const Mainpage = () => {
     return (
         <>
             <AiAssistant />
+            <BrainPower />
+            <Meeting />
         </>
     )
 }
