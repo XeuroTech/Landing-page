8005fcc0cdab33d76aaec7c0438b03093020c0dc
chiqrar <chiqrar6461@gmail.com>
2025-05-17 03:38:09 +0500

    commit

--- src/Components/MainPageCom/Footer.jsx
+++ src/Components/MainPageCom/Footer.jsx
@@ -1,11 +1,11 @@
-import { 
-  Box, 
-  Button, 
-  styled, 
-  Typography, 
-  TextField, 
-  Divider, 
-  Link 
+import {
+  Box,
+  Button,
+  styled,
+  Typography,
+  TextField,
+  Divider,
+  Link,
 } from "@mui/material";
 import React from "react";
 import { FoterBg } from "../../../Theme/ThemeProvider.jsx";
@@ -28,7 +28,7 @@ const FooterColumn = styled(Box)(() => ({
 }));
 
 const FooterTitle = styled(Typography)({
-  fontWeight: 600,
+  fontWeight: 500,
   color: "white",
   fontSize: "14px",
   marginBottom: "8px",
@@ -37,6 +37,7 @@ const FooterTitle = styled(Typography)({
 const FooterLink = styled(Link)({
   color: "#efedfd99",
   fontSize: "13px",
+  
   fontWeight: 500,
   textDecoration: "none",
 });
@@ -59,7 +60,7 @@ const Footer = () => {
   return (
     <FooterContainer>
       {/* Top Section */}
-      <Box 
+      <Box
         sx={{
           position: "relative",
           display: "flex",
@@ -103,168 +104,186 @@ const Footer = () => {
       </Box>
 
       {/* Footer Section */}
-      <Box px={{ xs: 2, lg: 6 }} py={4}>
-        {/* Main Footer Content */}
-        <Box
-          display="flex"
-          flexDirection={{ xs: "column", lg: "row" }}
-          justifyContent={{ xs: "flex-start", lg: "space-between" }}
-          alignItems={{ xs: "flex-start", lg: "flex-start" }}
-          gap={{ xs: 4, lg: 0 }}
-        >
-          {/* Left Side - Logo and Social */}
-          <Box
-            display="flex"
-            flexDirection={{ xs: "row", lg: "column" }}
-            justifyContent={{ xs: "space-between", lg: "flex-start" }}
-            gap={{ xs: 0, lg: "90px" }}
-            width={{ xs: "100%", lg: "auto" }}
-            mb={{ xs: 3, lg: 0 }}
-          >
-            <Box display="flex" gap={1} mb={{ lg: 3 }}>
-              <Box
-                component="img"
-                src={logo}
-                alt="Reflect logo"
-                width="30px"
-                height="30px"
-                sx={{ borderRadius: "8px" }}
-              />
-              <Typography fontWeight={600} color="#fff" fontSize="20px">
-                Reflect
-              </Typography>
-            </Box>
-            <Box display="flex" gap={2}>
-              <DiscordIcon sx={{ color: "#aaa" }} />
-              <TwitterIcon sx={{ color: "#aaa" }} />
-            </Box>
-          </Box>
+                                                <Box
+                                    width="100%"
+                                    px={{ xs: 1.25, sm: 2, md: 3, lg: 4, xl: 6 }}
+                                    py={4}
+                                    bgcolor="#030014"
+                                    sx={{ boxSizing: 'border-box' }}
+                                  >
+                                    {/* Main Footer Content */}
+                                    <Box
+                                      display="flex"
+                                      flexDirection={{ xs: "column", xl: "row" }}
+                                      justifyContent="space-between"
+                                      alignItems="flex-start"
+                                      maxWidth={{ lg: "310px", xl:"50%" }} // 👈 Core fix
+                                      width="100%"
+                                      mx="auto"
+                                      gap={4}
+                                    >
+                                      {/* Left: Logo + Social Icons */}
+                                      <Box
+                                        display="flex"
+                                        flexDirection={{ xs: "row", xl: "column" }}
+                                        justifyContent="space-between"
+                                        alignItems={{ xs: "center", xl: "flex-start" }}
+                                        gap={{ xs: 2, xl: 3 }}
+                                        width={{ xs: "100%", xl: "auto" }}
+                                        height={{xl:"210px",}}
+                                        minWidth={{ lg: "200px" }}
+                                      >
+                                        <Box display="flex" alignItems="center" gap={1}>
+                                          <Box
+                                            component="img"
+                                            src={logo}
+                                            alt="Reflect logo"
+                                            sx={{
+                                              width: "30px",
+                                              height: "30px",
+                                              borderRadius: "8px",
+                                              flexShrink: 0
+                                            }}
+                                          />
+                                          <Typography fontWeight={600} color="#fff" fontSize="20px" noWrap>
+                                            Reflect
+                                          </Typography>
+                                        </Box>
 
-          {/* Middle - Footer Links */}
-          <Box
-            display="flex"
-            flexDirection={{ xs: "column", lg: "row" }}
-            gap={{ xs: 4, lg: 8 }}
-            width={{ xs: "100%", lg: "auto" }}
-            justifyContent={{ xs: "flex-start", lg: "space-between" }}
-            ml={{ lg: 8 }}
-          >
-            <FooterColumn>
-              <FooterTitle>Product</FooterTitle>
-              <FooterLink href="#">Features</FooterLink>
-              <FooterLink href="#">Integrations</FooterLink>
-              <FooterLink href="#">Pricing</FooterLink>
-              <FooterLink href="#">Changelog</FooterLink>
-              <FooterLink href="#">Roadmap</FooterLink>
-            </FooterColumn>
+                                        <Box display="flex" gap={2}>
+                                          <DiscordIcon sx={{ color: "#aaa" }} />
+                                          <TwitterIcon sx={{ color: "#aaa" }} />
+                                        </Box>
+                                      </Box>
 
-            <FooterColumn>
-              <FooterTitle>Company</FooterTitle>
-              <FooterLink href="#">Our team</FooterLink>
-              <FooterLink href="#">Our values</FooterLink>
-              <FooterLink href="#">Blog</FooterLink>
-            </FooterColumn>
+                                      {/* Right: Footer Links */}
+                                      <Box
+                                        display="grid"
+                                        gridTemplateColumns={{
+                                          xs: "repeat(2, minmax(120px, 1fr))",
+                                          xl: "repeat(3, minmax(150px, 1fr))",
+                                        }}
+                                        gap={4}
+                                        flex={1}
+                                        width="100%"
+                                        textAlign="left"
+                                      >
+                                        <FooterColumn>
+                                          <FooterTitle>Product</FooterTitle>
+                                          <FooterLink href="#">Features</FooterLink>
+                                          <FooterLink href="#">Integrations</FooterLink>
+                                          <FooterLink href="#">Pricing</FooterLink>
+                                          <FooterLink href="#">Changelog</FooterLink>
+                                          <FooterLink href="#">Roadmap</FooterLink>
+                                        </FooterColumn>
 
-            <FooterColumn>
-              <FooterTitle>Resources</FooterTitle>
-              <FooterLink href="#">Downloads</FooterLink>
-              <FooterLink href="#">Documentation</FooterLink>
-              <FooterLink href="#">Contact</FooterLink>
-            </FooterColumn>
-          </Box>
-        </Box>
+                                        <FooterColumn>
+                                          <FooterTitle>Company</FooterTitle>
+                                          <FooterLink href="#">Our team</FooterLink>
+                                          <FooterLink href="#">Our values</FooterLink>
+                                          <FooterLink href="#">Blog</FooterLink>
+                                        </FooterColumn>
 
-        <Divider sx={{ backgroundColor: "#1a1a2e", my: 4 }} />
+                                        <FooterColumn>
+                                          <FooterTitle>Resources</FooterTitle>
+                                          <FooterLink href="#">Downloads</FooterLink>
+                                          <FooterLink href="#">Documentation</FooterLink>
+                                          <FooterLink href="#">Contact</FooterLink>
+                                        </FooterColumn>
+                                      </Box>
+                                    </Box>
 
-        {/* Newsletter Section */}
-        <Box
-          display="flex"
-          flexDirection={{ xs: "column", lg: "row" }}
-          justifyContent={{ xs: "flex-start", lg: "space-between" }}
-          alignItems={{ xs: "flex-start", lg: "center" }}
-          gap={3}
-        >
-          <Box width={{ xs: "100%", lg: "auto" }}>
-            <FooterTitle sx={{ fontSize: "18px" }}>
-              Get free note-taking workflows
-            </FooterTitle>
-            <Typography variant="body2" sx={{ color: "#efedfd99", fontSize: "14px" }}>
-              In our weekly newsletter.
-            </Typography>
-          </Box>
+                                    <Divider sx={{ backgroundColor: "#1a1a2e", my: 4 }} />
 
-          <Box
-            display="flex"
-            gap={1}
-            flexDirection={{ xs: "column", lg: "row" }}
-            alignItems={{ xs: "stretch", lg: "center" }}
-            width={{ xs: "100%", lg: "auto" }}
-          >
-            <TextField
-              variant="outlined"
-              placeholder="Enter your email"
-              size="small"
-              sx={{
-                width: { xs: "100%", lg: "300px" },
-                backgroundColor: "#9382FF0A",
-                borderRadius: "4px",
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                   border: "1px solid rgba(212, 212, 213, 0.6)",
-                    borderWidth: "1px",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#BDBDBD",
-                  },
-                  "& input::placeholder": {
-                    color: "#757575",
-                    opacity: 1,
-                  },
-                },
-                "& .MuiInputBase-input": {
-                  color: "white",
-                },
-              }}
-            />
-            <Button
-              variant="contained"
-              sx={{
-                width: { xs: "100%", lg: "auto" },
-                backgroundColor: "#9382FF0A",
-                color: "#fff",
-                fontWeight: 500,
-                borderRadius: "4px",
-                textTransform: "none",
-                border: "1px solid rgba(212, 212, 213, 0.6)",
-                whiteSpace: "nowrap",
-                px: 3,
-                py: 1,
-              }}
-            >
-              Subscribe
-            </Button>
-          </Box>
-        </Box>
+                                    {/* Newsletter Section */}
+                                    <Box
+                                      display="flex"
+                                      flexDirection={{ xs: "column", lg: "column" ,xl:"row"}}
+                                      justifyContent="space-between"
+                                      alignItems={{ xs: "flex-start", lg: "center" }}
+                                      gap={{xs:2, xl:"11rem" ,lg:2}}
+                                      maxWidth={{ xl: "max-content", lg: "310px" }}
+                                      width="100%"
+                                      mx="auto"
+                                      px={{  sm: 3, lg: 0 }}
+                                    >
+                                      <Box width="100%" textAlign={{ xs: "left", lg: "center" }}>
+                                        <FooterTitle sx={{ fontSize: { xs: "18px", lg: "22px" } }}>
+                                          Get free note-taking workflows
+                                        </FooterTitle>
+                                        <Typography variant="body2" sx={{ color: "#efedfd99", fontSize: "14px" }}>
+                                          In our weekly newsletter.
+                                        </Typography>
+                                      </Box>
 
-        <Divider sx={{ backgroundColor: "#1a1a2e", my: 4 }} />
+                                      <Box
+                                        display="flex"
+                                        gap={2}
+                                        flexDirection={{ xs: "column", sm: "row", lg: "column" ,xl:"row"}}
+                                        justifyContent={{ xs: "flex-start", sm: "center" }}
+                                        alignItems={{ xs: "stretch", sm: "center" }}
+                                        width={{ xs: "100%", lg: "auto"  }}
+                                      >
+                                        <TextField
+                                          variant="outlined"
+                                          placeholder="Enter your email"
+                                          size="small"
+                                          sx={{
+                                            width: { xs: "100%", sm: "280px", lg: "300px" },
+                                            backgroundColor: "#9382FF0A",
+                                            borderRadius: "4px",
+                                            "& .MuiOutlinedInput-root": {
+                                              "& fieldset": {
+                                                border: "1px solid rgba(212, 212, 213, 0.6)",
+                                              },
+                                              "& input": {
+                                                color: "white",
+                                              },
+                                            },
+                                          }}
+                                        />
+                                        <Button
+                                          variant="contained"
+                                          gap={2}
+                                          sx={{
+                                            width: { xs: "100%", sm: "auto", lg: "100%", xl:"max-content" },
+                                            px: { xs: 0, sm: 4, lg: 4 },
+                                            backgroundColor: "#9382FF0A",
+                                            color: "#fff",
+                                            border: "1px solid rgba(212, 212, 213, 0.6)",
+                                            whiteSpace: "nowrap",
+                                            flexShrink: 0,
+                                          }}
+                                        >
+                                          Subscribe
+                                        </Button>
+                                      </Box>
+                                    </Box>
 
-        {/* Bottom Links */}
-        <Box
-          display="flex"
-          flexDirection={{ xs: "column", lg: "row" }}
-          justifyContent={{ xs: "center", lg: "space-between" }}
-          alignItems="center"
-          gap={2}
-          textAlign={{ xs: "center", lg: "left" }}
-        >
-          <Typography variant="body2" color="#efedfd99">
-            Privacy Policy &nbsp; - &nbsp; Terms of Conditions
-          </Typography>
-          <Typography variant="body2" color="#efedfd99">
-            Reflect App, LLC. All rights reserved.
-          </Typography>
-        </Box>
-      </Box>
+                                    <Divider sx={{ backgroundColor: "#1a1a2e", my: 4 }} />
+
+                                    {/* Bottom Links */}
+                                    <Box
+                                      display="flex"
+                                      flexDirection={{ xs: "column", sm: "row", lg: "column" ,xl:"row" }}
+                                      justifyContent="space-between"
+                                      alignItems="center"
+                                      gap={2}
+                                      maxWidth={{ xl: "50%", lg: "310px"  }}
+                                      mx="auto"
+                                      width="100%"
+                                      textAlign="center"
+                                    >
+                                      <Typography variant="body2" color="#efedfd99">
+                                        Privacy Policy &nbsp; - &nbsp; Terms of Conditions
+                                      </Typography>
+                                      <Typography variant="body2" color="#efedfd99">
+                                        Reflect App, LLC. All rights reserved.
+                                      </Typography>
+                                    </Box>
+                                  </Box>
+
+
     </FooterContainer>
   );
 };
